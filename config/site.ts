28ea5f033a224fb6cb8dbb3d4aa2bda0e4a0d656
Vipul Kumar <next.vipul2@gmail.com>
2025-03-03 21:41:26 +0530

    fix issue

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Advacamp",
+  name: "PrimeFurniture",
   description: "Make beautiful websites regardless of your design experience.",
   navItems: [
     {
